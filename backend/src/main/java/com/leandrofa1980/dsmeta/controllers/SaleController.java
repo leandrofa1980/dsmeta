3ff7c306f605040f841d0a756bc902a9d709935d
leandrofa1980 <leandrofa1980@gmail.com>
2022-09-10 13:51:21 -0300

    Date update

--- backend/src/main/java/com/leandrofa1980/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/leandrofa1980/dsmeta/controllers/SaleController.java
@@ -3,7 +3,6 @@ package com.leandrofa1980.dsmeta.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +24,12 @@ public class SaleController {
 	private SmsService smsService;
 	
 	@GetMapping
-	public ResponseEntity<Page<Sale>> findSales(
-			@RequestParam(value="minDate", defaultValue = "") String minDate,
-			@RequestParam(value="maxDate", defaultValue = "") String maxDate,
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue ="") String minDate,
+			@RequestParam(value="maxDate", defaultValue ="") String maxDate,
 			Pageable pageable){
-		Page<Sale> list = service.findSales(minDate, maxDate, pageable);
-		return ResponseEntity.ok().body(list);
+		return service.findSales(minDate, maxDate, pageable);
+		
 	}
 	
 	@GetMapping("/{id}/notification")
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -7,20 +7,25 @@ import { BASE_URL } from '../../utils/request';
 import NotificationButton from '../NotificationButton';
 import './styles.css';
 
-const SalesCard = () => {
+function SalesCard() {
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
   const max = new Date();
 
-  const [minDate, setMinDate] = useState(min);
+  const [minDate, SetMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
 
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((response) => {
-      setSales(response.data.content);
-    });
-  }, []);
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then((response) => {
+        setSales(response.data.content);
+      });
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
@@ -29,7 +34,7 @@ const SalesCard = () => {
         <div className="dsmeta-form-control-container">
           <DatePicker
             selected={minDate}
-            onChange={(date: Date) => setMinDate(date)}
+            onChange={(date: Date) => SetMinDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
@@ -67,7 +72,7 @@ const SalesCard = () => {
                   <td>{sale.sellerName}</td>
                   <td className="show992">{sale.visited}</td>
                   <td className="show992">{sale.deals}</td>
-                  <td>{sale.amount.toFixed(2)}</td>
+                  <td>R$ {sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
                       <NotificationButton />
@@ -81,6 +86,6 @@ const SalesCard = () => {
       </div>
     </div>
   );
-};
+}
 
 export default SalesCard;
